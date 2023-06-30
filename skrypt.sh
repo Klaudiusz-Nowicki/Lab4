5e1e8d38cc276aec248eebc1987b4bfd5c4a6e38
Klaudiusz-Nowicki <klaudiusznowickiiiii@gmail.com>
2023-06-30 23:31:51 +0200

    zad3

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,13 @@
-
-
 #!/bin/bash
 
-if [ "$1" = "--date" ]; then
-    current_date=$(date +%F)
-    echo "Today is: $current_date"
-else
-    echo "Try: skrypt.sh --date"
+if [[ "$1" == "--date" ]]; then
+  echo "Today's date: $(date)"
+elif [[ "$1" == "--logs" ]]; then
+  for ((i=1; i<=100; i++))
+  do
+    filename="log${i}.txt"
+    echo "Filename: $filename" >> "$filename"
+    echo "Script name: script.sh" >> "$filename"
+    echo "Date: $(date)" >> "$filename"
+  done
 fi
